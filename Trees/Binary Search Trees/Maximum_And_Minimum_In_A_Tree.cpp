009b04ede718d38dee16c42b06cea9b7a1f21611
Samuvin jenish <samuvin.j@gmail.com>
2024-03-02 11:16:22 +0530

    Maximum_And_Minimum_In_A_Tree.cpp

---
+++ Trees/Binary Search Trees/Maximum_And_Minimum_In_A_Tree.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+    int findMax(Node *root)
+    {
+        if(root==NULL)
+            return INT_MIN;
+        int left=findMax(root->left);
+        int right=findMax(root->right);
+        return max({root->data,left,right});
+    }
+    int findMin(Node *root)
+    {
+        if(root==NULL)
+            return INT_MAX;
+        int left=findMin(root->left);
+        int right=findMin(root->right);
+        return min({root->data,left,right});
+    }
+};
