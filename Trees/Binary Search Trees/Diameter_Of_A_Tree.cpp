e586d37aae7717a086cdf2395b0ac61153deceaa
Samuvin jenish <samuvin.j@gmail.com>
2024-03-08 09:56:55 +0530

    Update Diameter_Of_A_Tree.cpp

--- Trees/Binary Search Trees/Diameter_Of_A_Tree.cpp
+++ Trees/Binary Search Trees/Diameter_Of_A_Tree.cpp
@@ -1,19 +1,37 @@
 
 class Solution {
 public:
+
     int diameterOfBinaryTree(TreeNode* root) 
     {
         int res=0;
-        auto DFS=[&](auto self,TreeNode* root)->int
+        auto Dfs_Height=[](auto self,TreeNode* root)->int
         {
             if(root==NULL)
                 return 0;
-            int lh=self(self,root->left);
-            int rh=self(self,root->right);
-            res=max(res,lh+rh);
-            return 1+max(lh,rh);
+            return 1+max(self(self,root->left),self(self,root->right));
         };
-        DFS(DFS,root);
+        auto DFS_Brute=[&](auto self,TreeNode* root) 
+        {
+            if(root==NULL)
+                return ;
+            int left=Dfs_Height(Dfs_Height,root->left);
+            int right=Dfs_Height(Dfs_Height,root->right);
+            res=max(res,left+right);
+            self(self,root->left);
+            self(self,root->right);
+        };
+        auto DFS_Optimal=[&res](auto self,TreeNode* root)
+        {
+            if(root==NULL)
+                return 0;
+            int left_child=self(self,root->left);
+            int right_child=self(self,root->right);
+            res=max(res,left_child+right_child);
+            return 1+max(right_child,left_child);
+        };
+        //DFS_Brute(DFS_Brute,root);
+        DFS_Optimal(DFS_Optimal,root);
         return res;
     }
 };
