f4f4821d5b606c0deb7b36e9d105ab8dc4d291ae
Samuvin jenish <samuvin.j@gmail.com>
2024-08-24 14:45:33 +0530

    Update Adjacency_Matrix.java

--- Implementation/Graph/Adjacency_Matrix.java
+++ Implementation/Graph/Adjacency_Matrix.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
-public class Graph {
+class Graph {
     private int V;
     private int E;
-    private boolean[][] adj;
+    public boolean[][] adj;
+
     public Graph(int V)
     {
         this.V=V;
@@ -13,19 +14,23 @@ public class Graph {
         adj[u][v] = true;
         adj[v][u] = true;
     }
-    public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
-        int V=sc.nextInt();
-        int E=sc.nextInt();
-        Graph g=new Graph(V);
-        for(int i=0;i<E;i++){
-            int u=sc.nextInt();
-            int v=sc.nextInt();
-            g.addEdge(u,v);
+}
+
+public class Adjacency_Matrix {
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int V = sc.nextInt();
+        int E = sc.nextInt();
+        Graph g = new Graph(V);
+        for (int i = 0; i < E; i++) {
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            g.addEdge(u, v);
         }
-        for(int i=1;i<=V;i++){
-            for(int j=1;j<=V;j++){
-                System.out.print(g.adj[i][j]+" ");
+        for (int i = 1; i <= V; i++) {
+            for (int j = 1; j <= V; j++) {
+                System.out.print(g.adj[i][j] + " ");
             }
             System.out.println();
         }
