2311ccdfad9b897ce61965487af24aae9128600b
Samuvin jenish <samuvin.j@gmail.com>
2024-10-07 20:16:19 +0530

    Update Count_Prefix_and_Suffix_Pairs_II.cpp

--- Trie/Count_Prefix_and_Suffix_Pairs_II.cpp
+++ Trie/Count_Prefix_and_Suffix_Pairs_II.cpp
@@ -1,3 +1,38 @@
+// class Node{
+// public:
+//     unordered_map<int,Node*>links;
+//     int count;
+//     Node()
+//     {
+//         count=0;
+//     }
+// };
+// class Solution {
+// Node* root;
+// public:
+//     Solution()
+//     {
+//         root=new Node();
+//     }
+//     long long countPrefixSuffixPairs(vector<string>& words) {
+//         long long res=0;
+//         for(string &str:words)
+//         {
+//             int len=str.size();
+//             Node* node=root;
+//             for(int i=0;i<str.size();i++)
+//             {
+//                 int hash=(str[i]*128)+str[len-i-1];
+//                 if(node->links.find(hash)==node->links.end())
+//                     node->links[hash]=new Node();
+//                 node=node->links[hash];
+//                 res+=node->count;
+//             }
+//             node->count+=1;
+//         }
+//         return res;
+//     }
+// };
 class Node{
 public:
     unordered_map<int,Node*>links;
@@ -22,7 +57,7 @@ public:
             Node* node=root;
             for(int i=0;i<str.size();i++)
             {
-                int hash=(str[i]*128)+str[len-i-1];
+                int hash=((str[i]-'a')*26)+(str[len-i-1]-'a');
                 if(node->links.find(hash)==node->links.end())
                     node->links[hash]=new Node();
                 node=node->links[hash];
