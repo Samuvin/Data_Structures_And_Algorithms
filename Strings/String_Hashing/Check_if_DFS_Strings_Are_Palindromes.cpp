0cd4e88ff2a33af32fd78248948199bda4dbcac6
Samuvin jenish <samuvin.j@gmail.com>
2024-10-20 21:39:24 +0530

    Update Check_if_DFS_Strings_Are_Palindromes.cpp

--- Strings/String_Hashing/Check_if_DFS_Strings_Are_Palindromes.cpp
+++ Strings/String_Hashing/Check_if_DFS_Strings_Are_Palindromes.cpp
@@ -1,63 +1,65 @@
 class Solution {
 public:
-	vector<bool> vis;
-	vector<vector<int>> adj;
+	vector<bool> isPalindrome;
+	vector<vector<int>> adjacencyList;
 
-	vector<long long>forward;
-	vector<long long>backward;
-	vector<long long>pow;
-	vector<long long>len;
+	vector<long long> forwardHash;
+	vector<long long> backwardHash;
+	vector<long long> power;
 
-	const long long int RADIX1 = 911;
-	const long long int MOD_1 = 1e9 + 7;
+	const long long int RADIX = 911;
+	const long long int MOD = 1e9 + 7;
 
-	void dfs(int src, string &s)
+	int computeForwardHash(int node, string& s)
 	{
-		len[src] = 1;
-		long long val = 0;
-
-		for (int i : adj[src])
+		int subtreeLength = 1;
+		for (int child : adjacencyList[node])
 		{
-			dfs(i, s);
-			forward[src] = ((forward[src] * pow[len[i]]) % MOD_1 + forward[i]) % MOD_1;
-			len[src] += len[i];
+			int childLength = computeForwardHash(child, s);
+			forwardHash[node] = ((forwardHash[node] * power[childLength]) % MOD + forwardHash[child]) % MOD;
+			subtreeLength += childLength;
 		}
-
-		forward[src] = (forward[src] * RADIX1 + (s[src] - 'a' + 1)) % MOD_1;
-		backward[src] = s[src] - 'a' + 1;
-
-		for (int node = adj[src].size() - 1; node >= 0; node--)
-		{
-			int child = adj[src][node];
-			backward[src] = (backward[src] * pow[len[child]] + backward[child]) % MOD_1;
-		}
+		forwardHash[node] = (forwardHash[node] * RADIX + (s[node] - 'a' + 1)) % MOD;
+		return subtreeLength;
 	}
 
-	vector<bool> findAnswer(vector<int>& parent, string s)
-	{
-		int n = s.size();
-		vis = vector<bool>(n, false);
-		forward.assign(s.size(), 0);
-		backward.assign(s.size(), 0);
-		adj.assign(n, vector<int>());
-		pow.assign(s.size(), 0);
-		len.assign(s.size(), 0);
-
-		pow[0] = 1;
-		for (int i = 1; i < s.size(); i++)
+	int computeBackwardHash(int node, string& s) {
+		int subtreeLength = 1;
+		backwardHash[node] = (backwardHash[node] + (s[node] - 'a' + 1)) % MOD;
+		for (int idx = adjacencyList[node].size() - 1; idx >= 0; idx--)
 		{
-			pow[i] = (pow[i - 1] * RADIX1) % MOD_1;
+			int child = adjacencyList[node][idx];
+			int childLength = computeBackwardHash(child, s);
+			backwardHash[node] = ((backwardHash[node] * power[childLength]) % MOD + backwardHash[child]) % MOD;
+			subtreeLength += childLength;
+		}
+		return subtreeLength;
+	}
+
+	vector<bool> findAnswer(vector<int>& parent, string s) {
+		int n = s.size();
+		isPalindrome = vector<bool>(n, false);
+		forwardHash.assign(n, 0);
+		backwardHash.assign(n, 0);
+		adjacencyList.assign(n, vector<int>());
+		power.assign(n, 0);
+
+		power[0] = 1;
+		for (int i = 1; i < n; i++)
+		{
+			power[i] = (power[i - 1] * RADIX) % MOD;
 		}
 
 		for (int i = 1; i < parent.size(); i++)
 		{
-			adj[parent[i]].push_back(i);
+			adjacencyList[parent[i]].push_back(i);
 		}
 
-		dfs(0, s);
+		computeForwardHash(0, s);
+		computeBackwardHash(0, s);
 
-		for (int i = 0; i < s.size(); i++)
-			vis[i] = (forward[i] == backward[i]);
-		return vis;
+		for (int i = 0; i < n; i++)
+			isPalindrome[i] = (forwardHash[i] == backwardHash[i]);
+		return isPalindrome;
 	}
 };
