1d9b9f5f60d5a35ec90f705fe555a1a615810f20
Samuvin jenish <samuvin.j@gmail.com>
2024-10-21 15:34:55 +0530

    Update Maximum_Length_of_Repeated_Subarray.cpp

--- Strings/Rabin_Karp/Single_Hash/Maximum_Length_of_Repeated_Subarray.cpp
+++ Strings/Rabin_Karp/Single_Hash/Maximum_Length_of_Repeated_Subarray.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
-
 	const long long RADIX = 101;
 	const long long MOD = 1e9 + 37;
-
 	vector<long long> computeRollingHash(vector<int>& arr, int length) 
-    {
+    	{
 		long long hash = 0;
 		long long max_power = 1;
 		for (int i = 0; i < length; i++) 
-        {
+        	{
 			max_power = (max_power * RADIX) % MOD;
 		}
 		for (int i = 0; i < length; i++) 
-        {
+        	{
 			hash = (hash * RADIX + arr[i]) % MOD;
 		}
 		vector<long long> hashValues;
 		hashValues.push_back(hash);
 		for (int i = 1; i <= (arr.size() - length); i++) 
-        {
+        	{
 			hash = (hash * RADIX - arr[i - 1] * max_power + MOD) % MOD; 
 			hash = (hash + arr[i + length - 1] + MOD) % MOD; 
 			hashValues.push_back(hash);
@@ -34,7 +32,7 @@ public:
 		set<long long> hashSet(hashA.begin(), hashA.end());
 
 		for (long long hashValue : hashB) 
-        {
+        	{
 			if (hashSet.find(hashValue) != hashSet.end())
 				return true;
 		}
