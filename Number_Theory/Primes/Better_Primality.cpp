f5da521641051e1b6f62cda0a035f1b4ba462dc7
Samuvin jenish <samuvin.j@gmail.com>
2024-10-23 14:42:22 +0530

    Create Better_Primality.cpp

---
+++ Number_Theory/Primes/Better_Primality.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+bool isPrime(long long num)
+{
+	if (num <= 1)
+		return false;
+	for (int i = 2; i * i < num; i++)
+	{
+		if (num % i == 0)
+			return false;
+	}
+	return true;
+}
+
+int main()
+{
+	cout << isPrime(36);
+}
